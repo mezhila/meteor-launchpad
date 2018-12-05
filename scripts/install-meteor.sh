9944bf2e57891e65d91566ff0c23f360dee1add7
mezhila <37409132+mezhila@users.noreply.github.com>
2018-12-05 07:03:22 +0530

    meteor release changes

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -6,10 +6,10 @@ if [ "$DEV_BUILD" = true ]; then
   # if this is a devbuild, we don't have an app to check the .meteor/release file yet,
   # so just install the latest version of Meteor
   printf "\n[-] Installing the latest version of Meteor...\n\n"
-  curl -v https://install.meteor.com/ | sh
+  curl -v https://install.meteor.com/?release=1.6.1.3 | sh
 else
   # download installer script
-  curl -v https://install.meteor.com -o /tmp/install_meteor.sh
+  curl -v https://install.meteor.com/?release=1.6.1.3 -o /tmp/install_meteor.sh
 
   # read in the release version in the app
   METEOR_VERSION=$(head $APP_SOURCE_DIR/.meteor/release | cut -d "@" -f 2)
